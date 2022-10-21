1a5a0fa652639ab617f47b1660349201ccaaa4a1
alunoctg <alunoctg@fiern.local>
2022-10-21 11:49:32 -0300

    login

--- Cuttie_Cuts/Salaoapp/urls.py
+++ Cuttie_Cuts/Salaoapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('docad/', views.docad, name='docad'),
     path('agendamento/', views.agendamento, name='agendamento'),
